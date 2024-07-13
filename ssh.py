f8d33cea5b1993ec391989cf1262f05bdc09b788
lzhsterling <140582626+lzhsterling@users.noreply.github.com>
2024-07-13 11:24:23 +0800

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -15,6 +15,18 @@ def ssh_connect(host, username, password, bark):
         transport.connect(username=username, password=password)
         ssh_status = "SSH连接成功"
         print(f"SSH连接成功。")
+	
+	# Exécuter la commande pm2 resurrect
+        ssh = transport.open_channel(kind='session')
+        ssh.exec_command('./home/username/.npm-global/bin/pm2 resurrect')
+        stdout = ssh.makefile('rb', -1)
+        stderr = ssh.makefile_stderr('rb', -1)
+        output = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        if error:
+            print(f"Erreur lors de l'exécution de la commande : {error}")
+        else:
+            print(f"PM2保活成功，已重启.")
 
         if bark:
             url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} SSH连接成功。"
@@ -22,7 +34,8 @@ def ssh_connect(host, username, password, bark):
     except Exception as e:
         ssh_status = f"SSH连接失败，错误信息: {e}"
         print(f"SSH连接失败: {e}")
-
+       
+        
         if bark:
             url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} SSH连接失败，请检查账号和密码是否正确。"
             response = requests.get(url)
